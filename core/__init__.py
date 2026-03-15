2f0d6d8cb6c969ad4f0ef3e6d95abbdbfe57835f
Emogyreunion <mv7786986@gmail.com>
2026-03-15 16:39:22 +0300

    remove tracking of unnecessary and sensitive files

---
+++ core/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+
+def create_app():
+    '''
+    initialize the flask's application instance
+    return: the application instance
+    '''
+
+    app = Flask()
+
+    return app
