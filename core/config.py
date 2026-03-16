cf85af996b08914fd7563e15e0c39ea6739645a9
Emogyreunion <mv7786986@gmail.com>
2026-03-16 14:44:00 +0300

    add: create a function to store configuration settings

---
+++ core/config.py
@@ -0,0 +1,12 @@
+'''
+stores the application's configuration settings
+'''
+import os
+import load_dotenv from dotenv
+
+load_dotenv()
+
+
+class Config():
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    DATABASE_URI = os.getenv('DATABASE_URI')
