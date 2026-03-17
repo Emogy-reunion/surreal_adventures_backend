c26fd5db374ae143bf740eb649a7f712dbf1f272
Emogyreunion <mv7786986@gmail.com>
2026-03-17 20:31:24 +0300

    add: add an init method to initialize the table with data, add a method that hashes the password before it is stored to secure it

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
-from core import db
+from core import db, bcrypt
 from sqlalchemy.dialects.postgresql import UUID
-from datetime import datetime
+from datetime import datetime, timezone
 import uuid
 
 
@@ -19,3 +19,14 @@ class Users(db.model):
                             default=lambda: datetime.now(timezone.utc),
                             onupdate=lambda: datetime.now(timezone.utc)
                             )
+
+    def __init__(self, email, password):
+        '''
+        initializes the table with data
+        '''
+        self.email = email
+        self.password_hash = hash_password(password)
+
+
+        def hash_password(self, password):
+            return bcrypt.generate_password_hash(password).decode('utf-8')
