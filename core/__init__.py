4a82138790bac02e3be8242e59dc28cdaa969923
Emogyreunion <mv7786986@gmail.com>
2026-03-17 20:54:44 +0300

    fix: fix the NameError by correcting the typo in the imported class

--- core/__init__.py
+++ core/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 
-db = SQALchemy()
+db = SQLAchemy()
 bcrypt = Bcrypt()
 migrate = Migrate()
 
