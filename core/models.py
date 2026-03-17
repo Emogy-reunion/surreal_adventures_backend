f4f2c5b0f4e550255a3d8d1ae35dd7d8be8888f1
Emogyreunion <mv7786986@gmail.com>
2026-03-17 19:20:17 +0300

    add: add columns to store the user data, the email, password, when the user created the account and when it was modified

--- core/models.py
+++ core/models.py
@@ -1,4 +1,7 @@
 from core import db
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime
+import uuid
 
 
 
@@ -6,3 +9,13 @@ class Users(db.model):
     '''
     stores the user data
     '''
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime,
+                           default: lambda: datetime.now(timezone.utc)
+                           )
+    modified_at = db.Column(db.DateTime,
+                            default=lambda: datetime.now(timezone.utc),
+                            onupdate=lambda: datetime.now(timezone.utc)
+                            )
