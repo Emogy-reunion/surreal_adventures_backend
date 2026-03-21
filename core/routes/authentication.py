400628d3e31e7d8366114425538519e6e947c92d
Emogyreunion <mv7786986@gmail.com>
2026-03-21 13:00:57 +0300

    add: create a register route that will be used to create accounts by storing the user's email and password to the database

--- core/routes/authentication.py
+++ core/routes/authentication.py
@@ -1,6 +1,44 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from core.models import Users
 from core import db
+from werkzeug.datastructures import MultiDict
 
 
-auth = Blueprint(__name__)
+auth = Blueprint('auth', __name__)
+
+
+@auth.route("/register", methods=['POST'])
+def register():
+    '''
+    allows users to create accounts
+    saves the email and password to the database
+    return:
+        errors: form errors
+        error: on error
+        success: on success
+    '''
+    try:
+        json_data = request.get_json() or {}
+        form = RegistrationForm(formData=MultiDict(json_data))
+
+        if not form.validate():
+            return jsonify({"errors": form.errors}), 400
+
+        email = form.email.data.strip().lower()
+        password = form.password.data
+
+        user = Users.query.filter_by(email=email).first()
+
+        if user:
+            return jsonify({'error': 'We couldn’t create your account — this email is already in use.'}), 400
+        else:
+            new_user = Users(email=email, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            return jsonify({"success": 'Your account was created successfully. Enjoy your experience.'}), 201
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': 'An unexpected error occurred. Please try again!'}), 500
+
+
