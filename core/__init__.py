7bdab5f38a9ad6c00a5dc27e16f55556563389f8
Emogyreunion <mv7786986@gmail.com>
2026-03-23 19:49:23 +0300

    add: call the function that registers the blueprints and pass the application instance

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from core.config import Config
+from core.routes import register_blueprints
 from core.extensions import jwt, bcrypt, migrate, db
 
 def create_app():
@@ -19,5 +20,7 @@ def create_app():
     migrate.init_app(app, db)
 
     import core.models
+
+    register_blueprints(app)
 
     return app
