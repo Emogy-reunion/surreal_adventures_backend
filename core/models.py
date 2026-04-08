2e2f36b666b5c523f90600f2acb5530e6c38fac4
Emogy-reunion <mv7786986@gmail.com>
2026-04-08 13:45:34 +0300

    add: index the email column to make querying faster

--- core/models.py
+++ core/models.py
@@ -16,7 +16,7 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    email = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False, index=True)
     password_hash = db.Column(db.String(100), nullable=False)
     created_at = db.Column(db.DateTime,
                            default=lambda: datetime.now(timezone.utc)
