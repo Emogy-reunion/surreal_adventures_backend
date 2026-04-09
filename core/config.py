b5a84b96ce6cae61c7aced0f526462fbaee33cf7
Emogy-reunion <mv7786986@gmail.com>
2026-04-10 00:15:02 +0300

    add: add configuration settings to configure the upload folder where uploads will go to

--- core/config.py
+++ core/config.py
@@ -7,6 +7,8 @@ from datetime import timedelta
 
 load_dotenv()
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config():
     SECRET_KEY = os.getenv('SECRET_KEY')
@@ -33,3 +35,5 @@ class Config():
 
     SUPER_ADMIN_EMAIL = os.getenv('SUPER_ADMIN_EMAIL')
     SUPER_ADMIN_PASSWORD = os.getenv('SUPER_ADMIN_PASSWORD')
+
+    UPLOAD_FOLDER =  os.path.join(BASE_DIR, 'static', 'uploads')
