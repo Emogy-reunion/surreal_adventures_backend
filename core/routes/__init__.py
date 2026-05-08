5427359b6c32a0841520deea071c61aa3c69e7fb
Emogyreunion <mv7786986@gmail.com>
2026-05-08 07:25:01 +0300

    add: register a blueprint that will be used to create routes that perform CRUD operations on tours

--- core/routes/__init__.py
+++ core/routes/__init__.py
@@ -1,8 +1,12 @@
 from core.routes.authentication import auth
+from core.routes.destinations import dest_bp
 
 
 def register_blueprints(app):
     '''
     registers the applications blueprints with the
     '''
-    app.register_blueprint(auth, url_prefix="/api/v1/auth")
+    app.register_blueprint(auth, url_prefix="/api/v1")
+    app.register_blueprint(dest_bp, url_prefix="/api/v1")
+    app.register_blueprint(tour_bp, url_prefix="/api/v1")
+
